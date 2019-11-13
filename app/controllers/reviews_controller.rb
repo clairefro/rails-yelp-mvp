db08992803868682bcf68849e9095dd0e74c14b4
claire froelich <claire.froelich@gmail.com>
2019-11-13 16:13:53 -0500

    reviews working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
 
+  # GET   /restaurants/:restaurant_id/reviews/new
+  def new
+    @rating_range = (0..5).to_a
+    @review = Review.new
+  end
+
+  # POST   /restaurants
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
